379852afa3ea67e5842c0dad0268c5bdc75656eb
Kevin Pestalozzi <kevin.pestalozzi@oopen-circle.ch>
2024-03-12 20:25:53 +0100

    test update

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 $PackageParameters = Get-PackageParameters
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$urlPackage = 'https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.5.0.0p1-Beta/OpenSSH-Win32-v9.5.0.0.msi'
-$urlPackage64 = 'https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.5.0.0p1-Beta/OpenSSH-Win64-v9.5.0.0.msi'
+$urlPackage = 'https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.4.0.0p1-Beta/OpenSSH-Win32-v9.4.0.0.msi'
+$urlPackage64 = 'https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.4.0.0p1-Beta/OpenSSH-Win64-v9.4.0.0.msi'
 $checksumPackage = '09bb63fe0005ac98826787a1b1b408d8f29c5d118df6c4a8382c79c269caac340d8ef70c2f9e47f58dd4e2c08a6365ad9a1601af588beddda55c3d83976a235e'
 $checksumPackage64 = ''
 $checksumTypePackage = 'SHA512'
